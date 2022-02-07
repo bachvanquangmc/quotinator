65b6896d334e34d4ec8baf81059834ac0f1f715f
ClaudiaShin <claudiashin07@gmail.com>
2022-02-07 00:14:02 -0800

    initial components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import react from 'react';
+import PageBtn from '../comps/PageBtn';
+import Sliders from '../comps/Slider';
+import Subheader from "../comps/Subheader";
+
 
 export default function Home() {
   return (
     <div>
-      
+      <PageBtn />
+      <div
+        style={{margin: 10}}
+      >
+        <Sliders />
+      </div>
     </div>
   )
 }
