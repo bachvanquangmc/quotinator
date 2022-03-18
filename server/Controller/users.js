aace9706dede0f824eb48ec1656297931f093d83
bachvanquangmc <bachvanquangmc@gmail.com>
2022-03-18 10:06:04 -0700

    signup works

---
+++ server/Controller/users.js
@@ -0,0 +1,34 @@
+const User = require('../Models/users');
+var jwt = require('jsonwebtoken');
+
+
+
+const signup = (req,res) => {
+// can also write as  const signup = ({ Body },res) => {}
+    const user = new User()
+    user.email = req.body.email
+    user.password = req.body.password
+    user.save((err,done)=>{
+        if(err) return res.status(500).send("Signup failed")
+        res.status(201).send('signed up successfully')
+    })
+}
+
+const login = (req,res) => {
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err || !user) return res.status(404).send('login failed')
+
+        // if(user.comparePassword(req.body.password)){
+        //     const token = jwt.sign({id:user._id}, 'thisismysecret');
+        //     res.send(token)
+        // } else {
+            res.send("could not login")
+        // }
+    })
+}
+
+
+module.exports = {
+    signup,
+    login
+}
