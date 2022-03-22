040e06f1f2b278f17c63a6c3b7453eecfffbc0b6
bachvanquangmc <bachvanquangmc@gmail.com>
2022-03-21 23:50:07 -0700

    update

--- server/Controller/favs.js
+++ server/Controller/favs.js
@@ -4,13 +4,6 @@ const createFavs = async (req, res) => {
   const body = req.body
   const fav = new Fav()
 
-  fav.quote = body.quote
-  fav.author = body.author
-  fav.save((err, data) => {
-    if (err) return res.status(400).send("not created")
-    res.status(201).send(data)
-  })
-
  fav.quote = body.quote
  fav.author = body.author
  fav.owner = body.owner
@@ -30,11 +23,18 @@ const getFavs = async (req, res)=> {
 }
 
 const getQuotes = async (req, res) => {
-  Fav.find(req.body.author,(err,favs)=>{
-    if(err) return res.status(500).send("no quotes found") 
-  })
+  const result = await Fav.find({author:req.params.author})
+  res.status(200).send(result) 
+   
+ }
+
+const deleteFavs = async (req, res) => {
+  const { id } = req.body
+  await Fav.findByIdAndRemove(id)
+
+  // res.send("deleted")
+
+  res.end()
 }
-
-
-module.exports = { createFavs, getFavs, getQuotes }
+module.exports = { createFavs, getFavs, getQuotes, deleteFavs }
 
--- server/Routes/favs.js
+++ server/Routes/favs.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { createFavs } = require('../Controller/favs');
 const { getFavs } = require('../Controller/favs');
 const { getQuotes } = require('../Controller/favs');
+const {deleteFavs} = require('../Controller/favs')
 
 router.post('/saved', createFavs)
 
@@ -11,6 +12,8 @@ router.get('/favs/:id', getFavs)
 router.get('/quotes/:author', getQuotes)
 
 router.get('/saved/:id', getFavs)
+
+router.delete('/delete/favs', deleteFavs)
 
 module.exports = router
 
