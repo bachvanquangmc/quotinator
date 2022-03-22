c6177904ac2fb5467e5cb32662408c6a382d3a1f
maggiesuu <msu28@my.bcit.ca>
2022-03-21 19:55:24 -0700

    get quotes back end

--- server/Routes/favs.js
+++ server/Routes/favs.js
@@ -2,8 +2,12 @@ const express= require('express')
 const router = express.Router()
 const { createFavs } = require('../Controller/favs');
 const { getFavs } = require('../Controller/favs');
+const { getQuotes } = require('../Controller/favs');
 
 router.post('/favs', createFavs)
 
 router.get('/favs/:id', getFavs)
+
+router.get('/quotes/:author', getQuotes)
+
 module.exports = router
