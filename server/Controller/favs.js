74cce19b840636b2d481da0e4369ce00df6b5f53
maggiesuu <msu28@my.bcit.ca>
2022-03-22 11:58:34 -0700

    changes

--- server/Controller/favs.js
+++ server/Controller/favs.js
@@ -8,10 +8,10 @@ const createFavs = async (req, res) => {
     owner: body.owner
   }).populate('owner')
 
- fav.save((err,data)=>{
-  if(err) return res.status(400).send("not created") 
-  res.status(201).send(data)
-})
+  fav.save((err, data) => {
+    if (err) return res.status(400).send("not created")
+    res.status(201).send(data)
+  })
 }
 
 const getFavs = async (req, res) => {
@@ -21,12 +21,11 @@ const getFavs = async (req, res) => {
   }).populate('owner', 'email -_id')
 }
 
-
 const getQuotes = async (req, res) => {
-  const result = await Fav.find({author:req.params.author})
-  res.status(200).send(result) 
-   
- }
+  const result = await Fav.find({ author: req.params.author })
+  res.status(200).send(result)
+
+}
 
 const deleteFavs = async (req, res) => {
   const { id } = req.body
--- server/Routes/favs.js
+++ server/Routes/favs.js
@@ -1,10 +1,6 @@
 const express= require('express')
 const router = express.Router()
-const { createFavs } = require('../Controller/favs');
-const { getFavs } = require('../Controller/favs');
-const { deleteFavs } = require('../Controller/favs');
-const { getQuotes } = require('../Controller/favs');
-const {deleteFavs} = require('../Controller/favs')
+const { createFavs, getFavs, deleteFavs, getQuotes } = require('../Controller/favs');
 const authoriseUser = require('../Auth/auth')
 
 router.post('/saved', authoriseUser, createFavs)
@@ -14,14 +10,8 @@ router.get('/favs/:id', authoriseUser, getFavs)
 // end point has to be diff from other, specific
 router.delete('/favs/delete', deleteFavs)
 
-module.exports = router
-
 router.get('/quotes/:author', getQuotes)
 
 router.get('/saved/:id', getFavs)
 
-router.delete('/delete/favs', deleteFavs)
-
 module.exports = router
-
-
