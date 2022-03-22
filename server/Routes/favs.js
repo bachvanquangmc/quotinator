6f88a663bf66d17a6a8295484f5e1c0a1e34fd8a
leighai <vnishibata@my.bcit.ca>
2022-03-21 19:57:58 -0700

    delete added to server controller and routes

--- server/Routes/favs.js
+++ server/Routes/favs.js
@@ -2,8 +2,14 @@ const express= require('express')
 const router = express.Router()
 const { createFavs } = require('../Controller/favs');
 const { getFavs } = require('../Controller/favs');
+const { deleteFavs } = require('../Controller/favs');
 
 router.post('/favs', createFavs)
 
 router.get('/favs/:id', getFavs)
+
+// end point has to be diff from other, specific
+router.delete('/favs/delete', deleteFavs)
+
+
 module.exports = router
